6a6195a5ae1fd1e335e4d17ee6a91229d2046ef1
Daniel Wehner <dawehner@googlemail.com>
2017-01-01 21:18:03 +0100

    uncomment non working api request

--- app/index.js
+++ app/index.js
@@ -26,20 +26,20 @@ class TinyUi extends Component {
   onSuccessfulUpload(image) {
     this.setState({imageUrl: image});
 
-    fetch(url, {
-      method: 'post',
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: 'foo=bar&lorem=ipsum'
-    })
-      .then(json)
-      .then(function (data) {
-        console.log('Request succeeded with JSON response', data);
-      })
-      .catch(function (error) {
-        console.log('Request failed', error);
-      });
+    // fetch(url, {
+    //   method: 'post',
+    //   headers: {
+    //     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    //   },
+    //   body: 'foo=bar&lorem=ipsum'
+    // })
+    //   .then(json)
+    //   .then(function (data) {
+    //     console.log('Request succeeded with JSON response', data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log('Request failed', error);
+    //   });
   }
 
   render() {
